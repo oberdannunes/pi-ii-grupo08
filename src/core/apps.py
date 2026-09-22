eb2c2dd2bf2e79d380e788a779e835f38fe919ee
Oberdan Nunes <oberdan.nunes@pca.com.br>
2026-09-21 22:06:26 -0300

    Tests do dashboard (alterações do Bruno)

--- src/core/apps.py
+++ src/core/apps.py
@@ -4,3 +4,17 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+
+    def ready(self):
+        # Correção de compatibilidade para Django 4.2 + Python 3.14.
+        # Centralizado no ready() do app core, para que seja aplicado em todos os testes e na execução normal.
+        from django.template.context import BaseContext
+
+        def patched_copy(self):
+            new_copy = self.__class__.__new__(self.__class__)
+            new_copy.__dict__.update(self.__dict__)
+            if hasattr(self, 'dicts'):
+                new_copy.dicts = self.dicts[:]
+            return new_copy
+
+        BaseContext.__copy__ = patched_copy
